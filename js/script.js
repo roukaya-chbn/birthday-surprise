536c0d28d81c3d60b75e5fe491cb3c0c2e00e7fd
roukaya-chbn <roukaya.chbn@gmail.com>
2026-08-02 14:54:02 +0100

    Add password check functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+function checkPassword(){
 
+    let password = document.getElementById("password-input").value;
+
+
+    if(password === "birthday"){
+
+        document.getElementById("password-page").style.display = "none";
+
+        document.getElementById("birthday-page").style.display = "flex";
+
+    }
+
+    else{
+
+        document.getElementById("error-message").innerHTML =
+        "Wrong password. Try again.";
+
+    }
+
+}
