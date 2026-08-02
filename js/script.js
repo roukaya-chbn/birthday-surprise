20896c06be642055717ffb11db5aef2be43b3997
roukaya-chbn <roukaya.chbn@gmail.com>
2026-08-02 15:49:59 +0100

    Add fireworks animation to canvas

--- js/script.js
+++ js/script.js
@@ -19,3 +19,104 @@ function checkPassword(){
     }
 
 }
+const canvas = document.getElementById("fireworks");
+
+const ctx = canvas.getContext("2d");
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+let particles = [];
+
+
+function createFirework(){
+
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height / 2;
+
+
+    for(let i = 0; i < 60; i++){
+
+        particles.push({
+
+            x:x,
+            y:y,
+
+            speedX:
+            (Math.random()-0.5)*8,
+
+            speedY:
+            (Math.random()-0.5)*8,
+
+            life:100
+
+        });
+
+    }
+
+}
+
+
+function animateFireworks(){
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+    particles.forEach((p,index)=>{
+
+
+        p.x += p.speedX;
+
+        p.y += p.speedY;
+
+        p.life--;
+
+
+        ctx.beginPath();
+
+        ctx.arc(
+            p.x,
+            p.y,
+            3,
+            0,
+            Math.PI*2
+        );
+
+
+        ctx.fillStyle =
+        "white";
+
+        ctx.fill();
+
+
+        if(p.life <= 0){
+
+            particles.splice(index,1);
+
+        }
+
+
+    });
+
+
+    requestAnimationFrame(
+        animateFireworks
+    );
+
+}
+
+
+setInterval(
+    createFirework,
+    1200
+);
+
+
+animateFireworks();
